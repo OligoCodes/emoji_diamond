50b52045e7d889dbfd97d96a07697b42ac238e18
Joseph Kwabena Osei Bonsu <josephoseibonsu742@gmail.com>
2025-07-26 20:25:50 +0000

    Update and rename stars.py to setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='emoji_diamond',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='Joseph Bonsu 🇬🇭',
+    description='A fun emoji diamond generator using classes in Python',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/OligoCodes/emoji_diamond',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
